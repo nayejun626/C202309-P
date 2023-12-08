ae2ca7a89308858c17c1d482272b558f24bf8cea
nayejun626 <skdpwns506@naver.com>
2023-12-08 18:54:36 +0900

    프로젝트

--- sources/code.c
+++ sources/code.c
@@ -1,80 +1,117 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-// 옷 스타일과 두께를 저장할 변수 선언
-char **clothStyle;
-char **clothThickness;
+// 옷의 종류와 두께를 멤버로 갖는 구조체 정의
+typedef struct
+{
+    char type[50];
+    int thickness;
+} Clothing;
 
-// 옷을 스타일과 두께에 따라 분류하는 함수
-void classifyClothing(char **styles, char **thickness, int inputClothes);
+// 온도에 따라 적절한 의상을 추천하는 함수 선언하기
+void recommendOutfit(int temperature, int thickness, char *type);
 
-// 온도에 따라 적절한 의상을 추천하는 함수
-void recommendOutfit(int temperature);
+// 옷을 분류하고 랜덤으로 타입을 선택하여 추천하는 함수
+void classifyAndRecommend(Clothing *clothes, int inputClothes, int temperature);
 
 int main()
 {
-    int temperature; // 온도를 저장할 변수
+    int temperature;
 
-    // 온도 정보를 입력 받는 부분 (실제로는 API를 통해 받아오겠지만, 여기서는 직접 입력 받음)
+    // 온도 정보를 입력 받는 부분 (실제로는 API를 통해 받아오겠지만, 여기에선 직접 입력 받음)
     printf("오늘의 온도는 몇 도인가요? ");
     scanf("%d", &temperature);
-
+    // 의류 데이터베이스가 없어서 번거롭지만, 일일이 옷을 입력 받음
     int inputClothes;
-    printf("가지고 있는 옷들의 수를 입력하세요: ");
+    printf("가지고 있는 옷의 수를 입력하세요: ");
     scanf("%d", &inputClothes);
-    // 온도에 따라 의상을 추천하는 함수 호출
-    recommendOutfit(temperature);
 
-        // 포인터 배열을 사용하여 동적으로 메모리 할당
-    clothStyle = (char **)malloc(inputClothes * sizeof(char *));
-    clothThickness = (char **)malloc(inputClothes * sizeof(char *));
+    // 구조체 배열 동적 할당
+    Clothing *clothes = (Clothing *)malloc(inputClothes * sizeof(Clothing));
 
-    // 각각의 옷 정보를 입력 받음
+    // 각각의 옷 정보를 입력 받음.
     for (int i = 0; i < inputClothes; ++i)
     {
-        clothStyle[i] = (char *)malloc(50 * sizeof(char));
-        clothThickness[i] = (char *)malloc(50 * sizeof(char));
-
         printf("옷 %d 정보 입력\n", i + 1);
-        printf("종류: ");
-        scanf("%s", clothStyle[i]);
-        printf("두께: ");
-        scanf("%s", clothThickness[i]);
+        printf("종류(패딩, 코트, 바람막이 등): ");
+        scanf("%s", clothes[i].type);
+
+        // 두께를 숫자로 입력받기
+        printf("두께( 1(매우두꺼움), 2(두꺼움), 3(중간), 4(얇음) 1~4 중 입력): ");
+        scanf("%d", &clothes[i].thickness);
     }
 
-    // 옷을 분류하는 함수 호출
-    classifyClothing(clothStyle, clothThickness, inputClothes);
+    // 옷을 분류하고 랜덤으로 타입을 선택하여 추천하는 함수 호출
+    classifyAndRecommend(clothes, inputClothes, temperature);
 
-    free(clothStyle); // 메모리 해제
-    free(clothThickness);
+    free(clothes); // 메모리 해제
 
     return 0;
 }
 
+// 옷을 분류하고 랜덤으로 타입을 선택하여 추천하기 위해 만든 함수
+// 이 함수가 수행하는 것은 1.배열에서 랜덤하게 옷 하나를 선택하고 ,
+// 2. 선택된 옷의 타입을 출력한 후 recommendOutfit 함수에 해당 정보(두께 및 타입)를 전달하는 것
+void classifyAndRecommend(Clothing *clothes, int inputClothes, int temperature)
+{
+    srand(time(NULL)); // 난수생성을 매번 다르게 하게끔 초기화 시켜줌
+
+    int randomType = rand() % inputClothes;
+
+    // 선택된 옷을 출력하는 문구
+    printf("추천된 옷: %s\n", clothes[randomType].type);
+
+    // recommendOutfit 함수를 호출해서 옷의 두께 및 종류를 인수로 전달한다.
+    recommendOutfit(temperature, clothes[randomType].thickness, clothes[randomType].type);
+}
+
 // 온도에 따라 의상을 추천하는 함수
-void recommendOutfit(int temperature)
+void recommendOutfit(int temperature, int thickness, char *type)
 {
     // 온도 범위에 따른 의상 추천
     if (temperature < 0)
     {
-        printf("~가 좋겠네요\n");
+        printf("오늘은 날이 매우 추우니 ");
+        // 추천 두께 설정
+        thickness = 1;
     }
     else if (temperature < 10)
     {
-        printf("\n");
+        printf("오늘은 날이 조금 춥습니다. ");
+        thickness = 2;
     }
     else if (temperature < 20)
     {
-        printf("\n");
+        printf("오늘은 날이 꽤 쌀쌀할 것 같아요. ");
+        thickness = 3;
     }
     else
     {
-        printf("\n");
+        printf("오늘은 따뜻한 날이 예상됩니다. ");
+        thickness = 4;
     }
-}
 
-// 옷을 분류하는 함수
-void classifyClothing(char **styles, char **thickness, int inputClothes)
-{
+    // 두께에 따라 옷을 추천
+    if (thickness == 1)
+    {
+        printf("두꺼운 %s를 추천합니다.\n", type);
+    }
+    else if (thickness == 2)
+    {
+        printf("따뜻한 %s를 추천합니다.\n", type);
+    }
+    else if (thickness == 3)
+    {
+        printf("가벼운 %s를 추천합니다.\n", type);
+    }
+    else if (thickness == 4)
+    {
+        printf("시원한 %s를 추천합니다.\n", type);
+    }
+    else
+    {
+        printf("잘못된 두께 입력값입니다.\n");
+    }
 }
